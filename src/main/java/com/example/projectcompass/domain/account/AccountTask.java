0f3b54b5ae0535391a5c32ac8018cd53eeca771b
KRBeomJunKim <wka8963@naver.com>
2022-02-09 23:54:21 +0900

    feat: Task, AccountTask Entity추가 Task Entity추가, Task와 account를 연결하는 중간 테이블 AccountTask Entity추가 feat #2

---
+++ src/main/java/com/example/projectcompass/domain/account/AccountTask.java
@@ -0,0 +1,23 @@
+package com.example.projectcompass.domain.account;
+
+import com.example.projectcompass.domain.common.Entity.BaseTimeEntity;
+import com.example.projectcompass.domain.project.Project;
+import com.example.projectcompass.domain.task.Task;
+
+import javax.persistence.*;
+
+@Entity
+public class AccountTask extends BaseTimeEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "account_task_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "account_id")
+    private Account account;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "task_id")
+    private Task task;
+}
