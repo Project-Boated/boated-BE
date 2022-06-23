23eb99664e85694f67aa3a7035a55d16e9d04e41
KRBeomJunKim <wka8963@naver.com>
2022-06-23 23:51:37 +0900

    hotfix: 배포된 서버에 CORS추가

--- src/main/java/org/projectboated/backend/web/infra/config/WebConfig.java
+++ src/main/java/org/projectboated/backend/web/infra/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins("http://localhost:3000", "http://localhost:8080", "http://15.164.89.188:3000")
+			.allowedOrigins("http://localhost:3000", "http://localhost:8080", "http://15.164.89.188:3000", "http://15.164.89.188/")
 				.allowedMethods(
 						HttpMethod.GET.name(),
 						HttpMethod.HEAD.name(),
