7b2ccb7f16c78413e9a425e7719fecac8627b567
KRBeomJunKim <wka8963@naver.com>
2022-06-25 14:28:25 +0900

    hotfix: login로직 수정

--- src/main/java/org/projectboated/backend/domain/account/repository/KakaoAccountRepository.java
+++ src/main/java/org/projectboated/backend/domain/account/repository/KakaoAccountRepository.java
@@ -3,11 +3,12 @@ package org.projectboated.backend.domain.account.repository;
 import org.projectboated.backend.domain.account.entity.KakaoAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface KakaoAccountRepository extends JpaRepository<KakaoAccount, Long> {
 
-	@Query("select ka from KakaoAccount ka left join fetch ka.roles")
-	Optional<KakaoAccount> findByKakaoId(Long kakaoId);
+	@Query("select ka from KakaoAccount ka left join fetch ka.roles where ka.kakaoId=:kakaoId")
+	Optional<KakaoAccount> findByKakaoId(@Param("kakaoId") Long kakaoId);
 }
