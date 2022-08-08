87f868880f793389b6ad308112e579d1e02e8c45
Sleepy-developer <wka8963@naver.com>
2022-08-08 23:22:11 +0900

    feat: taskfile 조회 close #167

--- src/test/java/com/projectboated/backend/web/task/taskfile/TaskFileControllerTest.java
+++ src/test/java/com/projectboated/backend/web/task/taskfile/TaskFileControllerTest.java
@@ -9,6 +9,7 @@ import com.projectboated.backend.infra.aws.AwsS3Service;
 import io.restassured.http.ContentType;
 import io.restassured.http.Cookie;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.util.ResourceUtils;
@@ -19,9 +20,9 @@ import java.util.stream.IntStream;
 import static com.projectboated.backend.common.data.BasicDataAccount.*;
 import static com.projectboated.backend.common.data.BasicDataProject.*;
 import static com.projectboated.backend.common.data.BasicDataTask.*;
-import static com.projectboated.backend.web.task.taskfile.document.TaskFileControllerDocument.documentTaskFileDelete;
-import static com.projectboated.backend.web.task.taskfile.document.TaskFileControllerDocument.documentUploadTaskFile;
+import static com.projectboated.backend.web.task.taskfile.document.TaskFileControllerDocument.*;
 import static io.restassured.RestAssured.given;
+import static org.mockito.Mockito.*;
 
 
 class TaskFileControllerTest extends AcceptanceTest {
@@ -69,6 +70,31 @@ class TaskFileControllerTest extends AcceptanceTest {
         .when()
             .port(port)
             .delete("/api/projects/{projectId}/tasks/{taskId}/files/{taskFileId}", projectId, taskIds[0], taskFileId)
+        .then()
+            .statusCode(HttpStatus.OK.value());
+    }
+
+    @Test
+    void getTaskFile_정상요청_정상return() throws FileNotFoundException {
+        // Given
+        when(awsS3Service.getBytes(any())).thenReturn("good".getBytes());
+
+        AccountTestUtils.createAccount(port, USERNAME, PASSWORD, NICKNAME, PROFILE_IMAGE_URL);
+        Cookie cookie = AccountTestUtils.login(port, USERNAME, PASSWORD);
+        int projectId = ProjectTestUtils.createProject(port, cookie, PROJECT_NAME, PROJECT_DESCRIPTION, PROJECT_DEADLINE);
+        int[] taskIds = IntStream.range(0, 5)
+                .map((i) -> TaskTestUtils.createTask(port, cookie, projectId, TASK_NAME + i, TASK_DESCRIPTION, TASK_DEADLINE))
+                .toArray();
+        int taskFileId = TaskFileTestUtils.createTaskFile(port, cookie, projectId, taskIds[0], ResourceUtils.getFile("classpath:profile-image-test.png"), "image/jpg");
+
+        // When
+        // Then
+        given(this.spec)
+            .filter(documentTaskFileRetrieve())
+            .cookie(cookie)
+        .when()
+            .port(port)
+            .get("/api/projects/{projectId}/tasks/{taskId}/files/{taskFileId}", projectId, taskIds[0], taskFileId)
         .then()
             .statusCode(HttpStatus.OK.value());
     }
