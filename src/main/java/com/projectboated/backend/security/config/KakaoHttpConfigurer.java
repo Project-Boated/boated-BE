c3bc73ddd4cbe76817f6dc63439c97af435d3cb1
Sleepy-developer <wka8963@naver.com>
2022-08-11 01:20:39 +0900

    hotfix: kakaoAuthenticationProvider 빠진부분 추가

--- src/main/java/com/projectboated/backend/security/config/KakaoHttpConfigurer.java
+++ src/main/java/com/projectboated/backend/security/config/KakaoHttpConfigurer.java
@@ -3,6 +3,8 @@ package com.projectboated.backend.security.config;
 import com.projectboated.backend.security.filter.KakaoAuthenticationFilter;
 import com.projectboated.backend.security.handler.KakaoAuthenticationFailureHandler;
 import com.projectboated.backend.security.handler.KakaoAuthenticationSuccessHandler;
+import com.projectboated.backend.security.provider.JsonAuthenticationProvider;
+import com.projectboated.backend.security.provider.KakaoAuthenticationProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -15,11 +17,13 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class KakaoHttpConfigurer extends AbstractHttpConfigurer<KakaoHttpConfigurer, HttpSecurity> {
 
+    private final KakaoAuthenticationProvider kakaoAuthenticationProvider;
     private final KakaoAuthenticationSuccessHandler successHandler;
     private final KakaoAuthenticationFailureHandler failureHandler;
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
+        http.authenticationProvider(kakaoAuthenticationProvider);
         http.addFilterBefore(kakaoAuthenticationFilter(http), UsernamePasswordAuthenticationFilter.class);
     }
 
